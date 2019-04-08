85cf13fdfdcc0aae2bdbb27bc5bbcd2f3603ba2a
Clarekpd <Clarekpd@outlook.com>
2019-04-08 17:38:24 -0500

    fixes the main activity problems with nav_

--- app/src/main/java/com/example/giveback/MainActivity.java
+++ app/src/main/java/com/example/giveback/MainActivity.java
@@ -109,18 +109,11 @@ import java.util.ArrayList;
         // Handle navigation view item clicks here.
         int id = item.getItemId();
 
-        if (id == R.id.nav_camera) {
+        if (id == R.id.nav_pending_conformation) {
             // Handle the camera action
-        } else if (id == R.id.nav_gallery) {
+        } else if (id == R.id.nav_pending_pickup) {
 
-        } else if (id == R.id.nav_slideshow) {
-
-        } else if (id == R.id.nav_manage) {
-
-        } else if (id == R.id.nav_share) {
-
-        } else if (id == R.id.nav_send) {
-
+        } else if (id == R.id.nav_past_pickups) {
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
