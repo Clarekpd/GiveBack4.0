22c3e302fcf494647122b8f6cd42c1e9b0e4f989
Joe Mahoney <mahoneyxp@hotmail.com>
2019-04-22 19:05:14 -0500

    button pushability for donation request.

--- app/src/main/java/com/example/giveback/fragments/DonationRequestFragment.java
+++ app/src/main/java/com/example/giveback/fragments/DonationRequestFragment.java
@@ -1,14 +1,20 @@
 package com.example.giveback.fragments;
 
+import android.app.NotificationManager;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.NotificationCompat;
+import android.support.v7.widget.LinearLayoutCompat;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.example.giveback.DonorRecord;
 import com.example.giveback.DonorRecordAdapter;
@@ -32,6 +38,21 @@ public class DonationRequestFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         //you can set the title for your toolbar here for different fragments different titles
         getActivity().setTitle(title);
+
+        Button submitButton = (Button) view.findViewById(R.id.submit_button);
+        submitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(view.getContext(), "ahhhhhh.", Toast.LENGTH_SHORT).show();
+               /* NotificationCompat.Builder builder = new NotificationCompat.Builder(view.getContext())
+                        .setSmallIcon(R.drawable.connect)
+                        .setContentTitle("Request Notification")
+                        .setContentText("Your donation request has been accepted.");
+                // Add as notification
+                NotificationManager manager = (NotificationManager)view.getContext().getSystemService(Context.NOTIFICATION_SERVICE);
+                manager.notify(0, builder.build());*/
+            }
+        });
 
     }
 
